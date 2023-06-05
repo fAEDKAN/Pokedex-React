1fab215ee5aed2162c8363b37f44e07c6362e8ff
fAEDKAN <103541453+fAEDKAN@users.noreply.github.com>
2023-06-04 23:31:45 -0300

    add new font to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import { GlobalStyles } from "./globalStyles.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
